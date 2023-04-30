0ee57a13bc562a189c7cdd7b535c2f41611db505
Ozumah Ebenezer <ozumahe@gmail.com>
2023-04-30 23:47:02 +0100

    Image Preview Added

--- src/app/pages/add-product/add-product.component.ts
+++ src/app/pages/add-product/add-product.component.ts
@@ -1,31 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.css'],
 })
-export class AddProductComponent {
-  fileName: string = '';
-
+export class AddProductComponent implements OnInit {
   newProduct = new FormGroup({
     name: new FormControl(''),
     price: new FormControl(''),
     description: new FormControl(''),
   });
 
+  fileName: string = '';
+  imgBase64: string = '';
+  isRequest: boolean = false;
+
+  ngOnInit(): void {}
+
   handleSubmit() {
-    console.log(this.newProduct.value);
+    console.log({ ...this.newProduct.value, image: this.imgBase64 });
   }
 
-  convertFileToBase64(event: any) {
-    var file = event.target.files[0];
-    console.log(file);
+  convertFileToBase64(evt: any) {
+    var file: any = evt.target.files[0];
 
-    var reader = new FileReader();
-    reader.onloadend = function () {
-      console.log('RESULT', reader.result);
-    };
-    reader.readAsDataURL(file);
+    if (file) {
+      var reader = new FileReader();
+      reader.onload = this.setBase64.bind(this);
+      reader.readAsBinaryString(file);
+    }
+  }
+
+  setBase64(readerEvt: any) {
+    var binaryString = readerEvt.target.result;
+    this.imgBase64 = btoa(binaryString);
+    console.log(btoa(binaryString));
   }
 }
